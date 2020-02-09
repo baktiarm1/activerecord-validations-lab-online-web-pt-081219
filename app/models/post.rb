b6fba2890cb12d6e309e9653f286ce9747dcbf4a
Baktiar Miah <baktiar.m@gmail.com>
2020-02-09 21:03:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
    include ActiveModel::Validations
-   validates_with TitleValidator
+   #validates_with TitleValidator
    validates :content, length: { minimum: 250 }
    validates :summary, length: { maximum: 250 }
    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
